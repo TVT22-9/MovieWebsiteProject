c4a11a857f155994f1809aae3fa5de25068dc407
Petri Kilpelainen <hunterpete2002@gmail.com>
2023-11-17 06:20:23 +0200

    Added basic search function to the movie list while also adding TV shows.

--- client/src/components/MovieListComponent.js
+++ client/src/components/MovieListComponent.js
@@ -1,50 +1,93 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-let pageNum = 1;
-const MovieCard = ({ movie }) => {
-    return (
-      <div className="movie-card">
-        <img src={'https://image.tmdb.org/t/p/w200' + movie.poster_path} alt={movie.title} />
-        <h2>{movie.title}</h2>
-        <p>{movie.overview}</p>
-        <p>{movie.id}</p>
-      </div>
-    );
-  };
+import _debounce from 'lodash.debounce';
   
 const MovieListComponent = () => {
+    //All the neccessery ways to update the useEffect.
     const [data, setData] = useState(null);
     const [pageNum, setPageNum] = useState(1);
+    const [listState, setListState] = useState(1);
+    const [searchQuery, setSearchQuery] = useState('');
 
+    const MovieCard = ({ movie }) => {
+        //The movie card that shows all neccessery data in the movie list. Edit this to decide what data the list shows
+        // WARNING This component currently uses listState that is part of MovieListComponent. Should we move this to its own component we need to change how it receive data to work.
+        // For some reason movies return their name as title while TV shows return as name.
+        return (
+            <div className="movie-card">
+                <img src={'https://image.tmdb.org/t/p/w200' + movie.poster_path} alt={movie.title} />
+                <h2>{listState === 1 ? movie.title : movie.name}</h2>
+                <p>{movie.overview}</p>
+                <p>{movie.id}</p>
+            </div>
+        );
+    };
+    
+    //Basic system to change the page and change between Movies (state 1) and TV Shows (state 2)
     const nextPage = () => {
         setPageNum(prevPageNum => prevPageNum + 1);
     };
     const previousPage = () => {
         setPageNum(prevPageNum => (prevPageNum > 1 ? prevPageNum - 1 : 1));
     };
+    const setListStateFunc = (state) => {
+        setListState(state);
+    };
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+    }
     
+    useEffect(() => { 
+        const fetchData = async () => {
+            try {
+              let response;
+        
+              if (searchQuery) {
+                response = await (listState === 1
+                  ? axios.get(`http://localhost:3001/api/searchMovie/${searchQuery}/${pageNum}`)
+                  : axios.get(`http://localhost:3001/api/searchShow/${searchQuery}/${pageNum}`));
+              } else {
+                response = await (listState === 1
+                  ? axios.get(`http://localhost:3001/api/topRatedMovies/${pageNum}`)
+                  : axios.get(`http://localhost:3001/api/topRatedShows/${pageNum}`));
+              }
+        
+              const resultsArray = Array.isArray(response.data.results) ? response.data.results : [];
+              setData(resultsArray);
+              console.log(response.data);
+            } catch (error) {
+              console.error('Error fetching data:', error);
+            }
+          };
+           // I read an old post of tmdb that said that calls faster than 50 per second are blocked as a ddos protection so i decided to add a delay to this just in case. If unneccesery then simply remove this and the import from top.
+           // In the case that we remove the delay we simply need to call fetchData.
+          const debouncedFetchData = _debounce(fetchData, 100);
+          debouncedFetchData();
+
+          return () => debouncedFetchData.cancel();
+    }, [pageNum, listState, searchQuery]);
     
-    useEffect(() => {
-        console.log('http://localhost:3001/api/topRated/' + pageNum)
-        axios.get('http://localhost:3001/api/topRated/' + pageNum) 
-            .then(response => {
-                const resultsArray = Array.isArray(response.data.results) ? response.data.results : [];
-                setData(resultsArray); 
-                console.log(response.data)})
-            .catch(error => console.error('Error fetching data:', error));
-      }, [pageNum]);
-    
-      return (
+
+    //Maps all returned data as MovieCard component. Gives all required data which can be assessed using movie.id and movie.title 
+    return (
         <div>
             <h1>Top Rated Movies</h1>
+            <button onClick={() => setListStateFunc(1)} disabled={listState === 1}>See movies list</button>
+            <button onClick={() => setListStateFunc(2)} disabled={listState === 2}>Next Page</button>
+
+            <input
+                type="text"
+                placeholder="Search..."
+                value={searchQuery}
+                onChange={(e) => handleSearch(e.target.value)}
+            />
+
             <div className="movie-grid">
-                {data?.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+                {data?.map(movie => <MovieCard key={movie.id} movie={movie} />)} 
             </div>
             <button onClick={previousPage} disabled={pageNum === 1}>Previous page</button>
             <button onClick={nextPage}>Next Page</button>
-
         </div>
-      );
+    );
 }
 export default MovieListComponent;
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -3,15 +3,15 @@ const apiOptions = require('../api_tools/api_options');
 const router = express.Router();
 
 // Movies below this
-router.get('/topRated/:page', async (req, res) => { // Get top rated movies per page.
+router.get('/topRatedMovies/:page', async (req, res) => { // Get top rated movies per page.
     try {
         const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page='+req.params.page;
         const response = await fetch(url, apiOptions);
         const result = await response.json();
         res.json(result);
     } catch (error) {
-      console.error('Error connecting to API', error);
-      res.status(500).send('Internal Server Error');
+        console.error('Error connecting to API', error);
+        res.status(500).send('Internal Server Error');
     }
 });
 router.get('/movieId/:id', async (req, res) => { // Gets movie using the id
@@ -21,21 +21,23 @@ router.get('/movieId/:id', async (req, res) => { // Gets movie using the id
         const result = await response.json();
         res.json(result);
     } catch (error) {
-      console.error('Error connecting to API', error);
-      res.status(500).send('Internal Server Error');
+        console.error('Error connecting to API', error);
+        res.status(500).send('Internal Server Error');
     }
 });
-router.get('/searchMovie/:query/:page', async (req, res) => { // Get top rated movies per page.
+router.get('/searchMovie/:query/:page', async (req, res) => { // Search Movies shows by name.
     try {
         const url = 'https://api.themoviedb.org/3/search/movie?query='+ req.params.query +'&language=en-US&page='+ req.params.page;
         const response = await fetch(url, apiOptions);
         const result = await response.json();
         res.json(result);
     } catch (error) {
-      console.error('Error connecting to API', error);
-      res.status(500).send('Internal Server Error');
+        console.error('Error connecting to API', error);
+        res.status(500).send('Internal Server Error');
     }
 });
+
+
 // TV shows below this
 
 router.get('/topRatedShows/:page', async (req, res) => { // Get top rated tv shows per page.
@@ -45,19 +47,31 @@ router.get('/topRatedShows/:page', async (req, res) => { // Get top rated tv sho
         const result = await response.json();
         res.json(result);
     } catch (error) {
-      console.error('Error connecting to API', error);
-      res.status(500).send('Internal Server Error');
+        console.error('Error connecting to API', error);
+        res.status(500).send('Internal Server Error');
     }
 });
-router.get('/tvShowId/:id', async (req, res) => { // Gets movie using the id
+router.get('/searchShow/:query/:page', async (req, res) => { // Search TV shows by name.
+    try {
+        const url = 'https://api.themoviedb.org/3/search/tv?query='+ req.params.query +'&language=en-US&page='+ req.params.page;
+        const response = await fetch(url, apiOptions);
+        const result = await response.json();
+        res.json(result);
+    } catch (error) {
+        console.error('Error connecting to API', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/tvShowId/:id', async (req, res) => { // Gets tv show using the id
     try {
         const url = 'https://api.themoviedb.org/3/tv/'+ req.params.id +'?language=en-US';
         const response = await fetch(url, apiOptions);
         const result = await response.json();
         res.json(result);
     } catch (error) {
-      console.error('Error connecting to API', error);
-      res.status(500).send('Internal Server Error');
+        console.error('Error connecting to API', error);
+        res.status(500).send('Internal Server Error');
     }
 });
 module.exports = router;
